fce2c1c7269041a173e5616b2eaa3525a6e8b24f
raeeschachar <raees.chachar@arbisoft.com>
2017-01-23 17:30:15 +0500

    Update utils.py

--- regression/pages/studio/utils.py
+++ regression/pages/studio/utils.py
@@ -128,7 +128,10 @@ def save_changes_popup_for_studio(self):
     Clicks Save button that displays on studio after addition/deletion
     """
     press_the_notification_button(self, "save")
-    self.wait_for_ajax()
+    self.wait_for_element_visibility(
+        '#alert-confirmation-title',
+        'Save confirmation message is visible'
+    )
 
 def click_confirmation_prompt_primary_button(self):
     """
