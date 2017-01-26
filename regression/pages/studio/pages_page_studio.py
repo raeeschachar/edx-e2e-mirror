a603e774c6a2631d58b9fb1a770bb727949fafd6
raeeschachar <raees.chachar@arbisoft.com>
2017-01-26 13:41:10 +0500

    Update pages_page_studio.py

--- regression/pages/studio/pages_page_studio.py
+++ regression/pages/studio/pages_page_studio.py
@@ -9,6 +9,7 @@ from regression.pages.studio.utils import (
     sync_on_notification
 )
 from regression.pages.studio.course_page_studio import CoursePageExtended
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class PagesPageExtended(CoursePageExtended):
@@ -149,13 +150,18 @@ class PagesPageExtended(CoursePageExtended):
         """
         toggle_checkbox_css = '.is-movable[data-tab-id="wiki"] ' \
                               '.action-visible [type="checkbox"]'
+        checkbox_css_action = self.q(
+            css='.is-movable[data-tab-id="wiki"] '
+                '.action-visible [type="checkbox"]'
+        ).results[0]
         self.wait_for_element_presence(
             toggle_checkbox_css, 'Toggle button presence'
         )
-        self.browser.execute_script(
-            "$('{}').click()".format(toggle_checkbox_css)
-        )
-        self.wait_for_ajax()
+        ActionChains(
+            self.browser
+        ).move_to_element(
+            checkbox_css_action
+        ).click(checkbox_css_action).perform()
         sync_on_notification(self)
         return 'Wiki'
 
